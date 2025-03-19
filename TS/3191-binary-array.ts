3c923590c428bee08a1fbb3632bb5c0e116f3338
Yash <yashagar0412@gmail.com>
2025-03-19 10:40:36 +0530

    3191

---
+++ TS/3191-binary-array.ts
@@ -0,0 +1,24 @@
+function minOperations(nums: number[]): number {
+  let operationsCount = 0;
+  let start = 0;
+
+  while (start < nums.length) {
+    if (nums[start] === 0) {
+      if (start > nums.length - 3) {
+        return -1;
+      }
+      operationsCount++;
+      let i = start;
+      while (i < start + 3) {
+        nums[i] = nums[i] === 1 ? 0 : 1;
+        i++;
+      }
+    }
+    start++;
+  }
+
+  return operationsCount;
+}
+
+console.log(minOperations([0, 1, 1, 1, 0, 0]));
+console.log(minOperations([0, 1, 1, 1]));
