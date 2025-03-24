6e49a5103dc47e6ffe97cb1d78b3e227c1e149fd
Yash <yashagar0412@gmail.com>
2025-03-24 11:15:08 +0530

    3169

---
+++ TS/3169-count-days-without-meetings.ts
@@ -0,0 +1,19 @@
+function countDays(days: number, meetings: number[][]): number {
+  let daysWithoutMeetings = 0;
+  let startDay = 1;
+  meetings.sort((a, b) => a[0] - b[0]);
+  for (let [start, end] of meetings) {
+    if (startDay < start) {
+      daysWithoutMeetings += start - startDay;
+    }
+    if (startDay <= end) {
+      startDay = end + 1;
+    }
+  }
+
+  if (startDay <= days) {
+    daysWithoutMeetings += days - startDay + 1;
+  }
+
+  return daysWithoutMeetings;
+}
